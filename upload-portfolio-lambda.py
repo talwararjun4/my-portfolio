8fe78e16e5367999637a0fa7e8c1a43dafb8bb37
talwararjun4 <talwararjun4@gmail.com>
2020-04-21 14:16:50 -0400

    Updated upload portfolio script again

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -7,18 +7,28 @@ import mimetypes
 
 def lambda_handler(event, context):
 
-    s3= boto3.resource('s3', config = Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:913426475809:portfolio-build-topic')
 
-    portfolio_bucket = s3.Bucket('portfolio.talwararjun4.com')
-    build_bucket = s3.Bucket('portfoliobuild.talwararjun4.com')
 
-    portfolio_zip  = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+    try:
+        s3= boto3.resource('s3', config = Config(signature_version='s3v4'))
+        portfolio_bucket = s3.Bucket('portfolio.talwararjun4.com')
+        build_bucket = s3.Bucket('portfoliobuild.talwararjun4.com')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj,nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip  = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
 
-    return 'Job Done!'
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print 'Job Done!'
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!")
+    except:
+        topic.publish(Subject="Portfolio Deployment Failed", Message="Portfolio was not deployed successfully!")
+        raise
+
+    return 'Completed!'
